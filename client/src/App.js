60702b5762abd867eff6201845d62edbff1dcc2e
Jenny Xin <jennyxin28@gmail.com>
2020-03-27 15:26:23 -0700

    fix anon listings

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,8 @@ class App extends React.Component {
     super(props);
 
     this.state = { 
-      user: ' ',
-      owner: ' ' 
+      user: 'anon',
+      owner: 'anon' 
     }
 
     this.responseGoogle = this.responseGoogle.bind(this);
@@ -33,7 +33,7 @@ class App extends React.Component {
 
   logout() {
     this.setState({
-      user: ''
+      user: 'anon'
     })
   }
 
